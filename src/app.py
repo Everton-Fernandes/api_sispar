38f68931aec896e24f49a1f5309c23a0cb00d31a
Everton-Fernandes <e.everton.elf@gmail.com>
2025-05-07 09:17:45 -0300

    first commit

---
+++ src/app.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from src.controller.colaborador_controller import bp_colaborador
+from src.model import db
+from config import Config
+from flask_cors import CORS
+from flasgger import Swagger
+
+swagger_config = {
+    "headers": [],  # cabeçalhos da requisição
+    "specs": [  # especificações da API lista + dicionário [{}]
+        {
+            "endpoint": "apispec",
+            "route": "/apispec.json",
+            "rule_filter": lambda rule: True,
+            "model_filter": lambda tag: True,
+        }
+    ],
+    "static_url_path": "/flasgger_static",
+    "swagger_ui": True,
+    "specs_route": "/apidocs/",
+}
+
+
+def create_app():
+    app = Flask(__name__)
+    CORS(app, origins="*")  # ativa o cors para todas as rotas
+    app.register_blueprint(bp_colaborador)
+    app.config.from_object(Config)
+    db.init_app(app)  # Inicializa o banco de dados com a aplicação Flask
+    Swagger(
+        app, config=swagger_config
+    )  # Inicializa o Swagger e adiciona a configuração
+    with app.app_context():
+        db.create_all()  # Cria as tabelas no banco de dados se não existirem
+    return app
---
+++ src/controller/reembolso_controller.py
@@ -0,0 +1,81 @@
+from flask import Blueprint, request, jsonify
+from src.model.reembolso_model import Reembolso
+from src.model import db
+from flasgger import swag_from
+
+bp_reembolso = Blueprint("reembolso", __name__, url_prefix="/reembolso")
+
+
+@bp_reembolso.route("/todos-reembolsos")
+def pegar_dados_todos_reembolsos():
+    reembolsos = db.session.execute(db.select(Reembolso)).scalars().all()
+    reembolsos = [reembolso.all_data() for reembolso in reembolsos]
+    return jsonify(reembolsos), 200
+
+
+@bp_reembolso.route("/cadastrar", methods=["POST"])
+@swag_from("../docs/reembolso/cadastrar_reembolso.yml")
+def cadastrar_novo_reembolso():
+    dados_requisicao = request.get_json()
+
+    novo_reembolso = Reembolso(
+        colaborador=dados_requisicao["colaborador"],
+        empresa=dados_requisicao["empresa"],
+        num_prestacao=dados_requisicao["num_prestacao"],
+        descricao=dados_requisicao["descricao"],
+        data=dados_requisicao["data"],
+        tipo_reembolso=dados_requisicao["tipo_reembolso"],
+        centro_custo=dados_requisicao["centro_custo"],
+        ordem_interna=dados_requisicao["ordem_interna"],
+        divisao=dados_requisicao["divisao"],
+        pep=dados_requisicao["pep"],
+        moeda=dados_requisicao["moeda"],
+        distancia_km=dados_requisicao["distancia_km"],
+        valor_km=dados_requisicao["valor_km"],
+        valor_faturado=dados_requisicao["valor_faturado"],
+        despesa=dados_requisicao["despesa"],
+        id_colaborador=dados_requisicao["id_colaborador"],
+        status=dados_requisicao["status"],
+    )
+
+    db.session.add(novo_reembolso)
+    db.session.commit()
+
+    return jsonify({"mensagem": "Dado cadastrado com sucesso"}), 201
+
+
+@bp_reembolso.route("/atualizar/<int:id_reembolso>", methods=["PUT"])
+def atualizar_dados_do_reembolso(id_reembolso):
+    dados_requisicao = request.get_json()
+
+    reembolso = db.session.execute(
+        db.select(Reembolso).where(Reembolso.id == id_reembolso)
+    ).scalar_one_or_none()
+
+    if not reembolso:
+        return jsonify({"mensagem": "Reembolso não encontrado"}), 404
+
+    for key, value in dados_requisicao.items():
+        setattr(reembolso, key, value)
+
+    db.session.commit()
+
+    return jsonify({"mensagem": "Dados do reembolso atualizados com sucesso"}), 200
+
+
+@bp_reembolso.route("/deletar/<int:id_reembolso>", methods=["DELETE"])
+def deletar_reembolso(id_reembolso):
+    reembolso = db.session.execute(
+        db.select(Reembolso).where(Reembolso.id == id_reembolso)
+    ).scalar_one_or_none()
+
+    if not reembolso:
+        return jsonify({"mensagem": "Reembolso não encontrado"}), 404
+
+    db.session.delete(reembolso)
+    db.session.commit()
+
+    return jsonify({"mensagem": "Reembolso deletado com sucesso"}), 200
+
+
+# db.session.bulk_save_objects([reembolso])
